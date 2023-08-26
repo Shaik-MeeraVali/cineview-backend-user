4048fa005f0a0aadcddf9c9af4bdcd61a756d573
Meera Vali Shaik <ME20379438@C02HG87MQ6L5>
2023-08-26 18:48:03 +0530

    first commit

---
+++ src/main/java/com/cinViewUser/ResponseType/GenericResponse.java
@@ -0,0 +1,19 @@
+package com.cinViewUser.ResponseType;
+
+public class GenericResponse {
+	
+	private String response;
+
+	public GenericResponse() {
+		super();
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+
+}
